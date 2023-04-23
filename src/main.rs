7e3277d4a90791d8d777d4a35847224e2d99f79e
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-04-24 04:08:40 +0900

    Add unit test for get_preamble() and build()

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,15 @@ use std::str;
 
 const THEME: &str = "default";
 
-fn build(args: Vec<String>) -> Result<(), Error> {
-    if args.len() != 2 {
-        // not one arg passed
-        println!("usage: scenic <file>");
-        process::exit(1);
-    }
-
+fn get_preamble(theme: &str) -> Result<String, Error> {
     let project_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
-    let theme_dir = project_dir.join("src").join("theme").join(THEME);
+    let theme_dir = project_dir.join("src").join("theme").join(theme);
     let preamble = fs::read_to_string(theme_dir.join("preamble.latex"))?;
+    Ok(preamble)
+}
 
-    let path = &args[1];
+fn build(path: &str, theme: &str) -> Result<String, Error> {
+    let preamble = get_preamble(theme)?;
     let content = fs::read_to_string(path)?;
 
     let latex = format!(
@@ -37,15 +34,95 @@ fn build(args: Vec<String>) -> Result<(), Error> {
     // returns an error for invalid UTF-8 sequence, and
     // `String::from_utf8_lossy()` generates invalid result.
     let data = unsafe { str::from_utf8_unchecked(&pdf_buf) };
-    print!("{}", data);
-
-    Ok(())
+    Ok(data.to_string())
 }
 
 fn main() {
-    let args = env::args().collect();
-    if let Err(e) = build(args) {
-        eprintln!("{e}");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        // not one arg passed
+        println!("usage: scenic <file>");
         process::exit(1);
+    }
+
+    let path = &args[1];
+    match build(path, THEME) {
+        Err(e) => {
+            eprintln!("{e}");
+            process::exit(1);
+        }
+        Ok(data) => {
+            print!("{}", data);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use std::io::ErrorKind;
+    use std::path::Path;
+
+    #[test]
+    fn test_preamble_not_found() {
+        let result = get_preamble("unknown");
+        assert!(result.is_err());
+
+        let err = result.err().unwrap();
+        assert_eq!(err.kind(), ErrorKind::NotFound);
+    }
+
+    #[test]
+    fn test_preamble() {
+        let expected = "\\documentclass{beamer}";
+        let result = get_preamble(THEME);
+
+        assert!(result.unwrap().contains(expected));
+    }
+
+    #[test]
+    fn test_build_invalid_theme() {
+        let project_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let test_data_dir = project_dir.join("test").join("data");
+
+        let input = test_data_dir.join("input.latex");
+        let path = input.to_str().unwrap();
+
+        let result = build(path, "unknown");
+        assert!(result.is_err());
+
+        let err = result.err().unwrap();
+        assert_eq!(err.kind(), ErrorKind::NotFound);
+    }
+
+    #[test]
+    fn test_build_missing_input_file_path() {
+        let project_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let test_data_dir = project_dir.join("test").join("data");
+
+        let input = test_data_dir.join("missing.latex");
+        let path = input.to_str().unwrap();
+
+        let result = build(path, THEME);
+        assert!(result.is_err());
+
+        let err = result.err().unwrap();
+        assert_eq!(err.kind(), ErrorKind::NotFound);
+    }
+
+    #[test]
+    fn test_build_broken_latex_input() {
+        let project_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let test_data_dir = project_dir.join("test").join("data");
+
+        let input = test_data_dir.join("invalid.latex");
+        let path = input.to_str().unwrap();
+
+        let result = build(path, THEME);
+        assert!(result.is_err());
+
+        let err = result.err().unwrap();
+        assert_eq!(err.kind(), ErrorKind::Other);
     }
 }
